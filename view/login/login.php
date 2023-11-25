d23149331d8e8bb287462c170accdac1265ccc5a
javier larrañaga <56646688+javier-sudo@users.noreply.github.com>
2023-11-24 21:02:43 -0300

    agrego la vista de login funcionando basico

---
+++ view/login/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../CSS/CssLogin.css">
+    <title>Iniciar Sesión</title>
+</head>
+<body>
+
+<div class="login-container">
+    <?php
+    require_once "../../controller/loginController.php";
+    // Mostrar mensajes de éxito o error
+    if (isset($resultadoMensaje)) {
+        echo "<p>$resultadoMensaje</p>";
+    }
+    ?>
+    <form action="" method="post"> <!-- Modificamos la acción del formulario -->
+        <h2>Iniciar Sesión</h2>
+        <label for="usu">Usuario:</label>
+        <input type="text" name="usu" required>
+
+        <label for="pass">Contraseña:</label>
+        <input type="password" name="pass" required>
+
+        <button type="submit" name="login-submit">Iniciar Sesión</button>
+    </form>
+
+    <div class="extra-options">
+        <a href="registro.php">Crear cuenta</a>
+        <a href="../../index.php">Volver al inicio</a>
+    </div>
+</div>
+
+</body>
+</html>
