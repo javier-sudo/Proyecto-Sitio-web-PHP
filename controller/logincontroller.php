aaab52dae816fd52e597314410f2b9a558aa8ee3
javier larrañaga <56646688+javier-sudo@users.noreply.github.com>
2023-11-25 13:47:49 -0300

    Add files via upload

--- controller/logincontroller.php
+++ controller/logincontroller.php
@@ -1,49 +1,24 @@
 <?php
-// Asegúrate de incluir LoginModel antes de usarlo
 require_once __DIR__ . '/../model/LoginModel.php';
-    class LoginController {
 
+class LoginController {
+    public function validarDatos() {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $usuario = $_POST["usu"];
+            $contrasena = $_POST["pass"];
 
-        
-        public function validarDatos() {
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $usuario = $_POST["usu"];
-                $contrasena = $_POST["pass"];
-    
-                // Crear una instancia del modelo
-                $loginModel = new LoginModel();
-    
-                // Validar datos
-                $resultado = $loginModel->validarDatos($usuario, $contrasena);
-    
-                // Manejar el resultado
-                if ($resultado['valido']) {
-                    echo "<p>Datos correctos. ¡Bienvenido!</p>";
-                    // Puedes redirigir aquí si es necesario
-                } else {
-                    echo "<p>{$resultado['mensaje']}</p>";
-                }
+            $loginModel = new LoginModel();
+
+            $resultado = $loginModel->validarDatos($usuario, $contrasena);
+
+            if ($resultado['valido']) {
+                echo "<p>Datos correctos. ¡Bienvenido!</p>";
+            } else {
+                echo "<p>{$resultado['mensaje']}</p>";
             }
         }
-    
-        // ...
-    
-    
-/*
-    public function cerrarSesion() {
-        // Iniciar sesión si no está iniciada
-        if (session_status() == PHP_SESSION_NONE) {
-            session_start();
-        }
-
-        // Destruir la sesión
-        session_destroy();
-
-        // Redirigir a la página de inicio de sesión
-        header("Location: ../view/login.php");
-        exit();
     }
-*/
+
     public function agregarCuenta() {
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $nombre = $_POST["nombre"];
@@ -53,19 +28,20 @@ require_once __DIR__ . '/../model/LoginModel.php';
             $estado = $_POST["estado"];
             $intentos = $_POST["intentos"];
 
+            $fotoFileName = '';
+
             // Verificar si se cargó una imagen
             if (isset($_FILES['foto']) && $_FILES['foto']['error'] === UPLOAD_ERR_OK) {
-                $foto = file_get_contents($_FILES['foto']['tmp_name']);
-            } else {
-                // Manejar el caso en que no se cargó una imagen
-                $foto = null;
+                $fotoTempPath = $_FILES['foto']['tmp_name'];
+                $fotoFileName = $_FILES['foto']['name'];
+                $fotoUploadPath = "tu_directorio_de_imagenes/" . $fotoFileName;
+
+                move_uploaded_file($fotoTempPath, $fotoUploadPath);
             }
 
-            // Crear una instancia del modelo
             $loginModel = new LoginModel();
 
-            // Agregar cuenta
-            $mensaje = $loginModel->agregarCuenta($nombre, $apellido, $usuario, $contrasena, $estado, $intentos, $foto);
+            $mensaje = $loginModel->agregarCuenta($nombre, $apellido, $usuario, $contrasena, $estado, $intentos, $fotoFileName);
             echo "<p>$mensaje</p>";
         }
     }
@@ -73,7 +49,6 @@ require_once __DIR__ . '/../model/LoginModel.php';
 
 $loginController = new LoginController();
 
-// Validar credenciales o agregar cuenta según la acción del formulario
 if (isset($_POST['login-submit'])) {
     $loginController->validarDatos();
 } elseif (isset($_POST['register-submit'])) {
